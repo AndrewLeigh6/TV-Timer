a23ca8c3dac84b06993c54a0ca74a29adddef623
Andrew <andrewleigh6@gmail.com>
2019-12-11 11:27:40 +0000

    Add logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Logo.module.css";
+
+const Logo = () => {
+  return (
+    <div className={classes.Logo}>
+      <h1>TV Timer</h1>
+    </div>
+  );
+};
+
+export default Logo;
