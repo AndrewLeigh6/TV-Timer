7e88e01264ba1969b3d2e1666e89e98a9656484d
Andrew <andrewleigh6@gmail.com>
2019-12-11 11:19:58 +0000

    Reset css and tidy folders

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Layout.module.css";
+
+const Layout = () => {
+  return (
+    <div className={classes.Layout}>
+      <p> I am the layout </p>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/containers/TvTimer/TvTimer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+class TvTimer extends Component {
+  render() {
+    return (
+      <Layout>
+        <p>Hello world</p>
+      </Layout>
+    );
+  }
+}
+
+export default TvTimer;
