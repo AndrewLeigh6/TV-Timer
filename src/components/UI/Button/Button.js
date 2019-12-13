d2f0e6eb94d461a1af92c767e76d96bad09a8c70
Andrew <andrewleigh6@gmail.com>
2019-12-13 14:57:55 +0000

    Improve button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,26 +6,60 @@ const Button = props => {
   const getClasses = () => {
     const buttonClasses = [classes.Button];
 
-    if (props.type === "primary") {
-      buttonClasses.push(classes.Primary);
+    if (props.maxWidth) {
+      buttonClasses.push(classes.MaxWidth);
     }
 
-    if (props.type === "secondary") {
-      buttonClasses.push(classes.Secondary);
+    switch (props.type) {
+      case "primary":
+        buttonClasses.push(classes.Primary);
+        break;
+      case "secondary":
+        buttonClasses.push(classes.Secondary);
+        break;
+      default:
+        break;
+    }
+
+    switch (props.size) {
+      case "small":
+        buttonClasses.push(classes.Small);
+        break;
+      case "medium":
+        buttonClasses.push(classes.Medium);
+        break;
+      case "large":
+        buttonClasses.push(classes.Large);
+        break;
+      default:
+        break;
     }
 
     return buttonClasses.join(" ");
   };
 
-  return (
-    <div className={getClasses()} onClick={props.clicked}>
-      {props.children}
-    </div>
-  );
+  switch (props.form) {
+    case true:
+      return (
+        <button className={getClasses()} onClick={props.clicked}>
+          {props.children}
+        </button>
+      );
+
+    default:
+      return (
+        <div className={getClasses()} onClick={props.clicked}>
+          {props.children}
+        </div>
+      );
+  }
 };
 
 Button.propTypes = {
-  type: PropTypes.oneOf(["primary", "secondary"])
+  type: PropTypes.oneOf(["primary", "secondary"]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  form: PropTypes.bool,
+  maxWidth: PropTypes.bool
 };
 
 export default Button;
