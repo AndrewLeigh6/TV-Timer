87cd0762507b60f27d993ece56c15f9fd8a99ea7
Andrew <andrewleigh6@gmail.com>
2019-12-13 15:05:00 +0000

    Add media selector

--- src/containers/TvTimer/TvTimer.js
+++ src/containers/TvTimer/TvTimer.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout/Layout";
 import TypeSelector from "../../components/TypeSelector/TypeSelector";
+import MediaSelector from "../../components/MediaSelector/MediaSelector";
 
 class TvTimer extends Component {
   render() {
     return (
       <Layout>
-        <TypeSelector />
+        <MediaSelector />
       </Layout>
     );
   }
