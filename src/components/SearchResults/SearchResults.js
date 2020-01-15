2838919c281f5bb74812d3855cdd14087327be8e
Andrew <andrewleigh6@gmail.com>
2020-01-15 10:14:39 +0000

    Convert SearchResults to class based component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import MediaDetails from "../MediaDetails/MediaDetails";
 import classes from "./SearchResults.module.css";
 
-const SearchResults = props => {
+class SearchResults extends Component {
   // sort by date descending
-  const sortFilms = films => {
+  sortFilms = films => {
     return films.sort(
       (a, b) => b.release_date.slice(0, 4) - a.release_date.slice(0, 4)
     );
   };
 
-  const formatFilms = films => {
+  formatFilms = films => {
     const PLACEHOLDER_POSTER_URL =
       "https://via.placeholder.com/500x750.png?text=No+poster+available";
     return films.map(film => {
       const id = film.id;
       const title = film.title;
       const poster = film.poster_path
-        ? `${props.getPosterBaseURL()}/${film.poster_path}`
+        ? `${this.props.getPosterBaseURL()}/${film.poster_path}`
         : PLACEHOLDER_POSTER_URL;
       const year = film.release_date.slice(0, 4);
 
@@ -27,9 +27,13 @@ const SearchResults = props => {
     });
   };
 
-  const films = props.films ? formatFilms(sortFilms(props.films)) : null;
+  render() {
+    const films = this.props.films
+      ? this.formatFilms(this.sortFilms(this.props.films))
+      : null;
 
-  return <div className={classes.SearchResults}>{films}</div>;
-};
+    return <div className={classes.SearchResults}>{films}</div>;
+  }
+}
 
 export default SearchResults;
