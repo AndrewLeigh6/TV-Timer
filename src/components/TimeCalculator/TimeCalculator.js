9905aeeaf15ff4e0fb9c6de441f52f61df8874b0
Andrew <andrewleigh6@gmail.com>
2020-01-30 13:49:58 +0000

    Add logic for film finish time string

--- src/components/TimeCalculator/TimeCalculator.js
+++ src/components/TimeCalculator/TimeCalculator.js
@@ -29,8 +29,33 @@ const TimeCalculator = props => {
     setStartTimeState(time);
   };
 
+  const getTimeDate = chosenTime => {
+    const chosenTimeHours = chosenTime.slice(0, 2);
+    const chosenTimeMinutes = chosenTime.slice(3);
+
+    const chosenTimeDate = new Date();
+    chosenTimeDate.setHours(chosenTimeHours);
+    chosenTimeDate.setMinutes(chosenTimeMinutes);
+
+    return chosenTimeDate;
+  };
+
   const calcFromStartTime = () => {
-    console.log("film duration is", currentMediaDetails.runtime);
+    const chosenTime = getTimeDate(startTime);
+
+    chosenTime.setMinutes(
+      chosenTime.getMinutes() + currentMediaDetails.runtime
+    );
+
+    /* this looks absolutely hideous, but it's just so
+    we can get the getMinutes result to look like 01, 02,
+    etc instead of 1, 2. */
+    const chosenTimeString =
+      chosenTime.getHours() +
+      ":" +
+      String(chosenTime.getMinutes()).padStart(2, "0");
+
+    console.log("Your film will end at ", chosenTimeString);
   };
 
   return (
